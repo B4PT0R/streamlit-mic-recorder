3f227d4d9862d781030a75d0322080d52fb5aa4f
B4PT0R <bferrand.maths@gmail.com>
2023-09-11 20:04:58 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit_mic_recorder",
-    version="0.0.1",
+    version="0.0.2",
     author="Baptiste Ferrand",
     author_email="bferrand.math@gmail.com",
     description="Streamlit component that allows to record mono audio from the user's microphone, and/or perform speech recognition directly.",
@@ -16,7 +16,7 @@ setuptools.setup(
     url="https://github.com/B4PT0R/streamlit-mic-recorder",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    classifiers=[],
+    classifiers=["streamlit","audio","STT"],
     python_requires=">=3.7",
     install_requires=[
         "streamlit >= 0.63",
