7cb2447174f3bf3e067ef928087d00506c86767c
B4PT0R <bferrand.maths@gmail.com>
2023-11-04 11:36:32 +0100

    Updated id management, added callbacks

--- streamlit_mic_recorder/__init__.py
+++ streamlit_mic_recorder/__init__.py
@@ -6,11 +6,6 @@ from speech_recognition import Recognizer,AudioData
 
 _RELEASE = True
 
-state=st.session_state
-
-if not '_last_audio_id' in state:
-    state._last_audio_id=0
-
 if not _RELEASE:
     _component_func = components.declare_component("streamlit_mic_recorder",url="http://localhost:3001")
 else:
@@ -18,33 +13,53 @@ else:
     build_dir = os.path.join(parent_dir, "frontend/build")
     _component_func = components.declare_component("streamlit_mic_recorder", path=build_dir)
 
-def mic_recorder(start_prompt="Start recording",stop_prompt="Stop recording",just_once=False,use_container_width=False,key=None):
+def mic_recorder(start_prompt="Start recording",stop_prompt="Stop recording",just_once=False,use_container_width=False,callback=None,args=(),kwargs={},key=None):
+    if not '_last_mic_recorder_audio_id' in st.session_state:
+        st.session_state._last_mic_recorder_audio_id=0
+    new_output=False
     component_value = _component_func(start_prompt=start_prompt,stop_prompt=stop_prompt,use_container_width=use_container_width,key=key,default=None)
     if component_value is None:
-        return None
+        output=None
     else:
         id=component_value["id"]
-        audio_bytes=base64.b64decode(component_value["audio_base64"])
-        sample_rate=component_value["sample_rate"]
-        sample_width=component_value["sample_width"]
-        if id>state._last_audio_id or just_once==False:
-            state._last_audio_id=id
-            return {"bytes":audio_bytes,"sample_rate":sample_rate,"sample_width":sample_width}
-        else: 
-            return None
+        new_output=(id>st.session_state._last_mic_recorder_audio_id)
+        if new_output or not just_once:
+            audio_bytes=base64.b64decode(component_value["audio_base64"])
+            sample_rate=component_value["sample_rate"]
+            sample_width=component_value["sample_width"]
+            output={"bytes":audio_bytes,"sample_rate":sample_rate,"sample_width":sample_width,"id":id}
+            st.session_state._last_mic_recorder_audio_id=id
+        else:
+            output=None
+    if key:
+        st.session_state[key]=output
+    if new_output and callback:
+        callback(*args,**kwargs)
+    return output
     
-def speech_to_text(start_prompt="Start recording",stop_prompt="Stop recording",just_once=False,use_container_width=False,language='en',key=None):
+def speech_to_text(start_prompt="Start recording",stop_prompt="Stop recording",just_once=False,use_container_width=False,language='en',callback=None,args=(),kwargs={},key=None):
+    if not '_last_speech_to_text_transcript_id' in st.session_state:
+        st.session_state._last_speech_to_text_transcript_id=0
     audio = mic_recorder(start_prompt=start_prompt,stop_prompt=stop_prompt,just_once=just_once,use_container_width=use_container_width,key=key)
+    new_output=False
     if audio is None:
-        return None
+        output=None
     else:
-        r = Recognizer()
-        audio_data = AudioData(audio['bytes'],audio['sample_rate'],audio['sample_width'])
-        try:
-            text = r.recognize_google(audio_data,language=language)
-            return text
-        except:
-            return ""
+        id=audio['id']
+        new_output=(id>st.session_state._last_speech_to_text_transcript_id)
+        if new_output or not just_once:
+            st.session_state._last_speech_to_text_transcript_id=id
+            r = Recognizer()
+            audio_data = AudioData(audio['bytes'],audio['sample_rate'],audio['sample_width'])
+            try:
+                output = r.recognize_google(audio_data,language=language)
+            except:
+                output=None
+    if key:
+        st.session_state[key]=output
+    if new_output and callback:
+        callback(*args,**kwargs)
+    return output
 
 if not _RELEASE:
     import streamlit as st
