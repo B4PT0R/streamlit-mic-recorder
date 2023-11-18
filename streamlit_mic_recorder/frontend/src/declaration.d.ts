3d151782d8e3cb1cd4c2b7a9d1ced5f524253f49
B4PT0R <bferrand.maths@gmail.com>
2023-11-18 10:30:14 +0100

    updated README

--- streamlit_mic_recorder/frontend/src/declaration.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'audiobuffer-to-wav';
-declare module 'tinycolor2'
-
-interface Window {
-    webkitAudioContext: typeof AudioContext;
-  }
--- streamlit_mic_recorder/frontend/src/react-app-env.d.ts
+++ streamlit_mic_recorder/frontend/src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+declare module 'audiobuffer-to-wav';
+declare module 'tinycolor2'
+
+interface Window {
+    webkitAudioContext: typeof AudioContext;
+  }
